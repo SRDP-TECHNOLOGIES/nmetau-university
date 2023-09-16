8cc130140edeec919c176a7edd37b580d991a837
Egor Serdyuk <volcanic-cupcake@proton.me>
2023-09-16 20:45:58 +0300

    Add faculty entity

---
+++ src/main/java/com/pdrs/nmetau/model/Faculty.java
@@ -0,0 +1,22 @@
+package com.pdrs.nmetau.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Faculty {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String iconPath;
+}
