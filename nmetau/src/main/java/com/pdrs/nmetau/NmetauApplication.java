b813472c299ea91a419202474ca687c72c80af62
Egor Serdyuk <volcanic-cupcake@proton.me>
2023-09-16 11:55:46 +0300

    initialize project

---
+++ nmetau/src/main/java/com/pdrs/nmetau/NmetauApplication.java
@@ -0,0 +1,13 @@
+package com.pdrs.nmetau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmetauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmetauApplication.class, args);
+	}
+
+}
