01450bb1368afaec316b5285db1bd824006a31b3
Egor Serdyuk <volcanic-cupcake@proton.me>
2023-09-17 10:54:13 +0300

    Faculty service interface

---
+++ src/main/java/com/pdrs/nmetau/service/FacultyService.java
@@ -0,0 +1,16 @@
+package com.pdrs.nmetau.service;
+
+import com.pdrs.nmetau.model.Faculty;
+import com.pdrs.nmetau.model.dto.FacultyDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FacultyService {
+    Optional<FacultyDTO> findFacultyById(int id);
+    List<FacultyDTO> getAllFaculties();
+    void createFaculty(Faculty faculty);
+    void updateFacultyById(int id, Faculty faculty);
+    void removeFacultyById(int id);
+}
