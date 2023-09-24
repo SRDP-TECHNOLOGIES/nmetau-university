f96d5a4d3731851388c2a6612a40b2250f5355d1
Yehor Serdiuk <yehor.serdiuk@proton.me>
2023-09-24 19:11:59 +0300

    Add data layer functionality

--- src/main/java/com/pdrs/nmetau/repository/NewsRepository.java
+++ src/main/java/com/pdrs/nmetau/repository/NewsRepository.java
@@ -1,7 +1,14 @@
 package com.pdrs.nmetau.repository;
 
 import com.pdrs.nmetau.model.News;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface NewsRepository extends PagingAndSortingRepository<News, Integer> {
+    List<News> findAll();
+    @Query("SELECT n from News n JOIN n.facultyList f where f.id = :facultyId")
+    List<News> findAllByFacultyId(@Param("facultyId") int facultyId);
 }
