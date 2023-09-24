bda52edb1869615352dbf0b146d1302f92099d68
Yehor Serdiuk <yehor.serdiuk@proton.me>
2023-09-24 19:12:19 +0300

    Naming tweaks in NewsService

--- src/main/java/com/pdrs/nmetau/service/NewsService.java
+++ src/main/java/com/pdrs/nmetau/service/NewsService.java
@@ -2,18 +2,18 @@ package com.pdrs.nmetau.service;
 
 import com.pdrs.nmetau.model.News;
 import com.pdrs.nmetau.model.dto.NewsDTO;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
 public interface NewsService {
-    List<NewsDTO> findAllNews();
-    List<NewsDTO> findAllNews(Pageable pageable);
-    List<NewsDTO> findAllNewsByFacultyId();
-    List<NewsDTO> findAllNewsByFacultyId(Pageable pageable);
-    Optional<NewsDTO> findNewsById();
-    void createNews(NewsDTO newsDTO);
-    void removeNewsById(int id);
-    void updateNewsById(int id, NewsDTO newsDTO);
+    List<NewsDTO> findAll();
+    List<NewsDTO> findAll(Pageable pageable);
+    List<NewsDTO> findAllByFacultyId(int facultyId);
+    List<NewsDTO> findAllByFacultyId(Pageable pageable);
+    Optional<NewsDTO> findById();
+    void create(NewsDTO newsDTO);
+    void removeById(int id);
+    void updateById(int id, NewsDTO newsDTO);
 }
